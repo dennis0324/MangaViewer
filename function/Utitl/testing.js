10be0c111ada1d67fa1339d4e5c3a5efe98554a0
dennis0324 <dennis0324@naver.com>
2022-03-29 00:05:21 +0900

    multiple loading complete

--- function/Utitl/testing.js
+++ function/Utitl/testing.js
@@ -1,177 +1,304 @@
 const testingJson = {
-  language_url: '/index-korean.html',
-  language: 'korean',
-  characters: null,
+  related: [ 2096145, 2096143, 2096140, 2027711, 2027709 ],
   files: [
     {
-      hasavif: 1,
+      name: '01.jpg',
+      height: 550,
       haswebp: 1,
-      height: 2000,
-      width: 1400,
-      hash: '0686a453a465a4b71da5aacc531820a04e0c23d20842bab4a51981ac861b0328',
-      name: '01.jpg'
+      hasavif: 1,
+      hash: '145f6f1a844167027b06ae26a352ce0aeb5e1f1f3d976c6aa55e2c0ff232daf5',
+      width: 394
     },
     {
-      height: 2000,
-      width: 1400,
+      haswebp: 1,
+      hasavif: 1,
+      hash: 'e1e27c1085bdfb54517f0466627acbb5e663d7e91ef6dd75c3fc0136d0480a43',
+      width: 2042,
       name: '02.jpg',
-      hash: '3932f22afa2cabe9509ce3ef000984f82cb41b78a807cc0d87d2a4f2cd1a45a3',
-      haswebp: 1,
-      hasavif: 1
+      height: 3029
     },
     {
-      hasavif: 1,
-      haswebp: 1,
-      height: 2000,
-      width: 1400,
+      height: 550,
       name: '03.jpg',
-      hash: 'a43d5813a9d37ba58b7b37801890b81f0ad93ca65a0d636a622741f05443e945'
+      width: 371,
+      hasavif: 1,
+      hash: '967043a6be704f086a21547bc26cce3693f8e2d996837920ac6cd35f2cfb85ed',
+      haswebp: 1
     },
     {
       haswebp: 1,
+      width: 372,
       hasavif: 1,
-      height: 2000,
-      width: 1400,
-      hash: '0160977dc2807cc92855aa3878daecb9edc510cb86d514595cf5fbeb3667f79d',
-      name: '04.jpg'
+      hash: 'd1b5a92adc68c836391020d5aee75e02985abfddd37b3a5c537d4dd22189670b',
+      name: '04.jpg',
+      height: 550
     },
     {
-      height: 2000,
+      haswebp: 1,
+      width: 372,
+      hash: '2599f94f28e54aa2f21fbd19032ca55a9131039815bbc08e3641d54932341c9c',
+      hasavif: 1,
       name: '05.jpg',
-      width: 1400,
-      hash: '2685f2a25bea9660e1ee3c9395fb15ee92c51410256fff690f884124f75d216d',
-      haswebp: 1,
-      hasavif: 1
+      height: 550
     },
     {
+      width: 370,
       hasavif: 1,
+      hash: '16dad08489650a0ba441417fb18cc60976039fbbb8f305853ea959fbd4f814b9',
       haswebp: 1,
-      height: 2000,
-      width: 1400,
-      hash: '46a5e5f6ee1908f8c6772196383fbcf7c305d8e12e2b6c1d1cf6b9528380825e',
+      height: 550,
       name: '06.jpg'
     },
     {
       haswebp: 1,
+      width: 371,
       hasavif: 1,
-      width: 1400,
-      height: 2000,
-      hash: '4bdb7997dc8fd100dfd9ae1da040e39f1e518a1c0981553e59fdcaf9c5faa49b',
-      name: '07.jpg'
+      hash: 'cfaa7ce5e58fbce2cb6129761b7e3fb79c1c7815161d6ccadc14d4ba2e32a02d',
+      name: '07.jpg',
+      height: 550
     },
     {
       hasavif: 1,
+      hash: '3b0be469f3d27a924bc47bdbff26fcbee8c86e48a7cf81f54fc642ac7e571448',
+      width: 373,
       haswebp: 1,
-      height: 2000,
-      width: 1400,
-      name: '08.jpg',
-      hash: 'e916eeb13a5c6368af7ff7dc62894aa2a6faebf37d601d6a487e71a87890e321'
+      height: 550,
+      name: '08.jpg'
     },
     {
-      haswebp: 1,
-      hasavif: 1,
-      width: 1400,
-      height: 2000,
+      height: 550,
       name: '09.jpg',
-      hash: 'bd616bf272b72c23a303aecd361670cbba7a7a001b5558e65fcf1495fbeba420'
+      width: 371,
+      hash: 'a014ce41a552aa8c7e7af66c937b5437d00b27212da3a799dac481c367dee3af',
+      hasavif: 1,
+      haswebp: 1
     },
     {
-      height: 2000,
-      width: 1400,
-      hash: 'd8781e5fa074a5469e9cba2a2e389eaf243a8efb63e267354525b677ad43de29',
+      haswebp: 1,
+      hasavif: 1,
+      hash: 'a1a792c60c6c702da8af47e43a5782ddf159f62acb02aaac5cc5c7d59e97a02b',
+      width: 372,
       name: '10.jpg',
-      hasavif: 1,
-      haswebp: 1
+      height: 550
     },
     {
-      haswebp: 1,
-      hasavif: 1,
-      height: 2000,
       name: '11.jpg',
-      width: 1400,
-      hash: '743cf7884ec6d9fc5b2c4fe6dee1f55c7c3b5bfab882a7e78c27274e0ea95ad2'
+      height: 550,
+      haswebp: 1,
+      hasavif: 1,
+      hash: '252417f25fb1041d2a7ab8f638f49149e1592276db36880fa1c4ed2c4259dec0',
+      width: 370
     },
     {
-      height: 2047,
-      width: 1447,
-      hash: 'eff0e61fb08c72978b7c996398dd1b6c81d1aa7a986d0277fbc04867bd940b0f',
       name: '12.jpg',
+      height: 550,
+      haswebp: 1,
+      width: 372,
+      hash: '9b2be3278b3a4bba19143a9414bb93380755c9d8ca248f392a19c56618cdb77e',
+      hasavif: 1
+    },
+    {
+      width: 371,
       hasavif: 1,
+      hash: '36845e47c675252b5c7701d4d02e952c69f0f442ab188f7e7ff58e7cce6f2349',
+      haswebp: 1,
+      height: 550,
+      name: '13.jpg'
+    },
+    {
+      haswebp: 1,
+      hash: '928bd38c41e5ee316190b567cbdbd926e44035e7460cc6a76c65d6b928c33aed',
+      hasavif: 1,
+      width: 373,
+      name: '14.jpg',
+      height: 550
+    },
+    {
+      height: 550,
+      name: '15.jpg',
+      hash: 'd9aa8ecf45038022bb037540f125b0bc3a37d8b149f896d5b1805a62f4f0ebfe',
+      hasavif: 1,
+      width: 371,
       haswebp: 1
     },
     {
+      hash: '1fb692deed107aab5539056a33eaa769f0d2b6e2195665124a2031ff0f6f82e7',
+      hasavif: 1,
+      width: 371,
+      haswebp: 1,
+      height: 550,
+      name: '16.jpg'
+    },
+    {
+      width: 372,
+      hash: '4bba96ee3af39150aa22e917ca2904f7065885460d9c10eb813526c445221963',
       hasavif: 1,
       haswebp: 1,
-      name: '13.jpg',
-      height: 2414,
-      width: 2400,
-      hash: '610b9ede2f58b195c107618961596113edf8ec76ea4ba4182d5f14ca068d9e4f'
+      height: 550,
+      name: '17.jpg'
+    },
+    {
+      haswebp: 1,
+      width: 373,
+      hasavif: 1,
+      hash: '0d264bb67a1fdb82327f66c14aa89c0fc4a6f24e654f65285469fe66a189cfe5',
+      name: '18.jpg',
+      height: 550
+    },
+    {
+      haswebp: 1,
+      width: 371,
+      hasavif: 1,
+      hash: '5edd92709ab68b806b384d3ec1149d4c281ff5286f97da82fddbce8ba54c366f',
+      name: '19.jpg',
+      height: 550
+    },
+    {
+      name: '20.jpg',
+      height: 550,
+      haswebp: 1,
+      width: 370,
+      hash: '75664636a8d9c263ca4bf540c8eefc0447b903901c022b015485266805354bd1',
+      hasavif: 1
+    },
+    {
+      haswebp: 1,
+      hasavif: 1,
+      hash: '2622f78905fa299307cfce5fb3462d68d58e135ce36cf8b1a718057944b70930',
+      width: 372,
+      name: '21.jpg',
+      height: 550
+    },
+    {
+      haswebp: 1,
+      width: 371,
+      hash: 'd2730f8d0e145c85ce5603997541edadb94359de5bdf805bfd0b86a50ab774bb',
+      hasavif: 1,
+      name: '22.jpg',
+      height: 550
+    },
+    {
+      height: 550,
+      name: '23.jpg',
+      width: 372,
+      hasavif: 1,
+      hash: '92f744e8e8c988385826cbd58cde9335dc8347b8e26ab6809c6d417b2e5cfcc6',
+      haswebp: 1
+    },
+    {
+      hash: '8bae67d314a5951e3d9d11557bc85024224d35804121cfe567ee892f2dc0394a',
+      hasavif: 1,
+      width: 373,
+      haswebp: 1,
+      height: 550,
+      name: '24.jpg'
+    },
+    {
+      width: 372,
+      hash: 'f3631986800119fca094e24c9b4de172be7bd93722ced9d663ed0028519751ad',
+      hasavif: 1,
+      haswebp: 1,
+      height: 550,
+      name: '25.jpg'
+    },
+    {
+      height: 550,
+      name: '26.jpg',
+      hash: '5701b914243f6711128edcaf173f84df5b7393229a3b76d41bf5b3e862628570',
+      hasavif: 1,
+      width: 371,
+      haswebp: 1
+    },
+    {
+      height: 550,
+      name: '27.jpg',
+      hash: '5d1899a672aea2b22e93d84c9dc5ece507a9e6bf927c7f069f9351a4590a6776',
+      hasavif: 1,
+      width: 372,
+      haswebp: 1
+    },
+    {
+      name: '28.jpg',
+      height: 550,
+      haswebp: 1,
+      width: 373,
+      hasavif: 1,
+      hash: '9c86dae2a7e1111384460a063b988ab99185c0451aeedb81b1e4572c343d3a7f'
+    },
+    {
+      height: 550,
+      name: '29.jpg',
+      hash: '385cc4b2801f039cafefd7f9aeb946db6f686e6543e7296826051b7c0a2b990c',
+      hasavif: 1,
+      width: 372,
+      haswebp: 1
+    },
+    {
+      width: 371,
+      hasavif: 1,
+      hash: 'daf8ea1ccd495b389d86c33d5bcac56d8011fbe887f2bcce7214071e96fc14cf',
+      haswebp: 1,
+      height: 550,
+      name: '30.jpg'
+    },
+    {
+      hash: 'c2ebeef10fdb7ee31212f1da7273b78f827d1676af8aee87fb1c950ad4bb80f7',
+      hasavif: 1,
+      width: 372,
+      haswebp: 1,
+      height: 550,
+      name: '31.jpg'
+    },
+    {
+      haswebp: 1,
+      hash: 'd53f562c925cbaa4d874a6175a962fdfb8d19dd586a6d53f2f6667a8d5405125',
+      hasavif: 1,
+      width: 372,
+      name: '32.jpg',
+      height: 550
+    },
+    {
+      height: 550,
+      name: '33.jpg',
+      hash: 'd1378e8ca09ef7730c223a8319d507a9ecd7902dae5058cdfacb8c094e9383c3',
+      hasavif: 1,
+      width: 372,
+      haswebp: 1
+    },
+    {
+      haswebp: 1,
+      width: 395,
+      hasavif: 1,
+      hash: '8c48636b3ba4328fee8df1931b24f492106fecd140133629db7ca04d7ded4943',
+      name: '34.jpg',
+      height: 550
+    },
+    {
+      haswebp: 1,
+      width: 1152,
+      hasavif: 1,
+      hash: 'e460608039f8140f313f7c9242037e8bd0a6d72712d993c9ba14eca8179bc864',
+      name: '35.jpg',
+      height: 768
     }
   ],
-  language_localname: '한국어',
-  languages: [],
-  artists: null,
-  tags: [
-    {
-      url: '/tag/female%3Abig%20breasts-all.html',
-      tag: 'big breasts',
-      male: '',
-      female: 1
-    },
-    {
-      url: '/tag/female%3Acatgirl-all.html',
-      tag: 'catgirl',
-      female: 1,
-      male: ''
-    },
-    {
-      url: '/tag/female%3Afurry-all.html',
-      tag: 'furry',
-      male: '',
-      female: 1
-    },
-    {
-      url: '/tag/female%3Atiger-all.html',
-      tag: 'tiger',
-      female: 1,
-      male: ''
-    },
-    {
-      female: '',
-      male: 1,
-      tag: 'furry',
-      url: '/tag/male%3Afurry-all.html'
-    },
-    {
-      url: '/tag/male%3Ahorse%20boy-all.html',
-      female: '',
-      male: 1,
-      tag: 'horse boy'
-    },
-    {
-      tag: 'human on furry',
-      female: '',
-      male: 1,
-      url: '/tag/male%3Ahuman%20on%20furry-all.html'
-    },
-    {
-      url: '/tag/male%3Apig%20man-all.html',
-      male: 1,
-      female: '',
-      tag: 'pig man'
-    }
-  ],
-  videofilename: null,
+  id: '2178960',
   video: null,
-  title: 'イチャラブ催眠アプリ',
-  related: [ 1915462, 1827841, 1523807, 1461063, 2157276 ],
-  date: '2022-03-27 03:40:00-05',
-  groups: null,
-  id: '2177645',
+  parodys: [ { url: '/series/naruto-all.html', parody: 'naruto' } ],
+  tags: [ { tag: 'scanmark', url: '/tag/scanmark-all.html' } ],
+  date: '2022-03-28 04:36:00-05',
+  language_url: '/index-japanese.html',
+  language_localname: '日本語',
   scene_indexes: [],
-  type: 'doujinshi',
-  parodys: [ { url: '/series/original-all.html', parody: 'original' } ],
-  japanese_title: null
+  artists: null,
+  characters: null,
+  japanese_title: null,
+  languages: [],
+  videofilename: null,
+  language: 'japanese',
+  title: '児戯とみだら',
+  groups: null,
+  type: 'doujinshi'
 }
 
 
@@ -230,16 +357,18 @@ function url_from_hash(galleryid, image, dir, ext) {
 note = []
 const testing = async () => {
 
-  let index = await hitomi.getIndex(1,5)
+  let index = await hitomi.getIndex(1,10)
   await Promise.all(index.nozomi.map(async (element) => {
     const response  = await hitomi.getGalleryInfo(element)
     let temp = JSON.parse(response.replace("var galleryinfo = ",""))
-    // console.log(temp)
+    console.log(temp)
     note.push(temp)
     // getImages(temp)
     // let image = await hitomi.getGalleryImg("https://aa.hitomi.la/webp/1648310402/45/3a233001e89f76599eb1e390366ef53336543b95c7d64ffecbd2b71985d192d0.webp")
     // console.log(image.data)
   }));
+
+  // console.log(note)
   return note
 
   // console.log(url_from_hash("",jsonTemp,'avif','avif'))
@@ -251,13 +380,11 @@ const testing = async () => {
 
 const getImages = async (listFiles) => {
   let listImage = []
-  let data = await hitomi.getRequest("https://ltn.hitomi.la/gg.js");
-  ggFunction = data.data.toString().replace("gg","const gg")
-  vm.runInThisContext(ggFunction)
+
 
   // await Promise.all(listFiles.files.map(async (element,index) => {
   //   let decodeUrl;
-  //   if(index > 5) {
+  //   if(index > 10) {
   //     return;
   //   }
   //   decodeUrl = common.url_from_url_from_hash(listFiles.id, element, 'webp', undefined, 'a')
@@ -268,35 +395,97 @@ const getImages = async (listFiles) => {
   //   listImage.push(image.data.toString('base64'));
   // }));
 
-  // getGalleryImgs(listFiles.files)
 
-  getGalleryImgs(testingJson)
 
+  // return listImage;
 }
 
-const testingImage = async (url) =>{
-  //https://aa.hitomi.la/webp/1648310402/45/3a233001e89f76599eb1e390366ef53336543b95c7d64ffecbd2b71985d192d0.webp
-  let image = await hitomi.getGalleryImg("https://aa.hitomi.la/webp/1648310402/45/3a233001e89f76599eb1e390366ef53336543b95c7d64ffecbd2b71985d192d0.webp")
-  // listImage.push(image.data.toString('base64'));
-  console.log(image.data.toString('base64'));
+// const testingImage = async (url) =>{
+//   //https://aa.hitomi.la/webp/1648310402/45/3a233001e89f76599eb1e390366ef53336543b95c7d64ffecbd2b71985d192d0.webp
+//   let image = await hitomi.getGalleryImg("https://aa.hitomi.la/webp/1648310402/45/3a233001e89f76599eb1e390366ef53336543b95c7d64ffecbd2b71985d192d0.webp")
+//   // listImage.push(image.data.toString('base64'));
+//   // console.log(image.data.toString('base64'));
+
+//   // await Promise.all(listFiles.files.map(async (element,index) => {
+//   //   let decodeUrl;
+//   //   if(index > 10) {
+//   //     return;
+//   //   }
+//   //   decodeUrl = common.url_from_url_from_hash(listFiles.id, element, 'webp', undefined, 'a')
+//   //   console.log(decodeUrl)
+//   //   let image = await hitomi.getGalleryImg(decodeUrl)
+//   //   console.log(index);
+//   //   // console.log(image.data) 
+//   //   listImage.push(image.data.toString('base64'));
+//   // }));
+// }
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const getGalleryImgs = async (files) => {
-  // 
-  // for()
-  count = 0
-  for(var file in files){
-    var radomDelay = parseInt(Math.random() * 3 + 1)
-    decodeUrl = common.url_from_url_from_hash(file.id, file, 'webp', undefined, 'a')
+const wrapSlept = async (ms) => { await sleep(ms) };
+
+const getGalleryImg = async(id,files) => {
+  console.log(files)
+  listImage = []
+  await Promise.all(files.map(async (file,index) => {
+    let decodeUrl;
+    decodeUrl = common.url_from_url_from_hash(id, file, 'webp', undefined, 'a')
     console.log(decodeUrl)
-    hitomi.getGalleryImg(decodeUrl).then(await wait(3000))
+    let image = await hitomi.getGalleryImg(decodeUrl)
+    console.log(index);
+    console.log(image.data) 
+    // listImage.push(image.data.toString('base64'));
+  }));
+  return listImage
+}
+
+const getGallery = async (listFiles) => {
+  start = new Date()
+  let data = await hitomi.getRequest("https://ltn.hitomi.la/gg.js");
+  ggFunction = data.data.toString().replace("gg","const gg")
+  vm.runInThisContext(ggFunction)
+
+  console.log(listFiles.files.length)
+  nodeArray = []
+  while(listFiles.files.length > 0){
+    nodeArray.push(listFiles.files.splice(0,5))
   }
+
+  console.log("testing :" + nodeArray)
+  failArray = []
+  for(const [index, fileNode] of nodeArray.entries()){
+    var radomDelay = parseInt(Math.random() * 3 + 1)
+    // console.log(fileNode)
+    // decodeUrl = common.url_from_url_from_hash(listFiles.id, fileNode, 'webp', undefined, 'a')
+    // let image = await hitomi.getGalleryImg(listFiles.id,decodeUrl)
+    // console.log(image.data);
+
+    // listFiles.files.slice(start, end);
+    try{
+      await getGalleryImg(listFiles.id,fileNode)
+    }
+    catch(e){
+      console.log(index)
+      failArray.push(index)
+    }
+    // await hitomi.getGalleryImg(decodeUrl)
+    await wrapSlept(radomDelay * 110)
+  }
+  end = new Date()
+  console.log((end - start) / 1000 + "초");
+  console.log(failArray)
 }
 //67f366f6a6973c1eb6e3220f232be003e99fc4f5a42693c7635789e3eaa11879
 // testing();
-getImages(testingJson)
-// testingImage();
 
+getGallery(testingJson)
+
+
+// getImages(testingJson)
+// getGalleryImgs(testingJson)
+// testingImage();
 
 // console.log(real_full_path_from_hash('67f366f6a6973c1eb6e3220f232be003e99fc4f5a42693c7635789e3eaa11879'))
 
